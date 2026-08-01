95f1fc512c7d10b08da59bf8276e79d2444b4e41
seva9523 <seva9523@gmail.com>
2026-08-01 11:10:34 +0100

    Use the monitored support form for legal contact

--- src/lib/legal.ts
+++ src/lib/legal.ts
@@ -1,5 +1,5 @@
 export const LEGAL_OPERATOR_NAME = 'ChicMagnolia';
-export const LEGAL_CONTACT_EMAIL = 'support@chicmagnolia.com';
+export const LEGAL_CONTACT_PATH = '/support';
 export const TERMS_VERSION = '2026-07-31';
 export const PRIVACY_VERSION = '2026-07-31';
 export const LEGAL_LAST_UPDATED = '31 July 2026';
