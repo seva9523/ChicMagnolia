04b3a947bbabbfc50606833f2432b3b3b2c2d360
seva9523 <seva9523@gmail.com>
2026-08-04 12:33:23 +0100

    Format supported retailer catalogue tests

--- src/retailers/catalog.test.ts
+++ src/retailers/catalog.test.ts
@@ -9,13 +9,11 @@ import {
 } from './catalog';
 
 const sampleUrls = {
-  Zara:
-    'https://www.zara.com/uk/en/bomber-jacket-with-dots-p08372236.html?v1=545479235&v2=2417772',
+  Zara: 'https://www.zara.com/uk/en/bomber-jacket-with-dots-p08372236.html?v1=545479235&v2=2417772',
   Mango:
     'https://shop.mango.com/gb/en/p/women/dresses-and-jumpsuits/dresses/flared-sleeve-satin-dress/27019066/79/00',
   Next: 'https://www.next.co.uk/style/sv098626/v86409',
-  ASOS:
-    'https://www.asos.com/asos-design/asos-design-contrast-lace-detail-v-neck-cami-in-grey-marl/prd/210111307#colourWayId-210111308',
+  ASOS: 'https://www.asos.com/asos-design/asos-design-contrast-lace-detail-v-neck-cami-in-grey-marl/prd/210111307#colourWayId-210111308',
   UNIQLO:
     'https://www.uniqlo.com/uk/en/products/E473791-000/01?colorDisplayCode=65&sizeDisplayCode=002',
   'H&M': 'https://www2.hm.com/en_gb/productpage.1265326001.html',
